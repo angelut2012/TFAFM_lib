2b704a46eb08540911e4f7831f82aac224326109
au <angelut2012@gmail.com>
2015-11-03 23:52:02 -0500

    feed forward model bug fixed

--- piezo_feed_forward.h
+++ piezo_feed_forward.h
@@ -48,7 +48,7 @@ double data_polyFit(int axis, double position_input )
 //	double deltaPosition = PositionInput - PositionNow[axis];
 //	double temp_PositionInput=PositionInput;
 //	double model_outputV=0;
-//	uint32_t value_dac=0;
+//	uint32_t value_dac[axis]=0;
 //	//double max_step_size=max(MAX_STEP_SIZE_PIEZO_MODEL,(deltaPosition/MAX_STEPS_PIEZO_MODEL+0.001));
 //	double max_step_size=max(MAX_STEP_SIZE_PIEZO_MODEL,(deltaPosition/steps+0.001));
 //
@@ -92,12 +92,12 @@ double data_polyFit(int axis, double position_input )
 //
 //			deltaPosition = PositionInput - PositionNow[axis];
 //			//if (axis>0)
-//			value_dac=(uint32_t)(model_outputV*(double)BIT18MAX);
-//			DAC_write(axis, value_dac);
+//			value_dac[axis]=(uint32_t)(model_outputV*(double)BIT18MAX);
+//			DAC_write(axis, value_dac[axis]);
 //
 //
-//			//Serial.print("value_dac ");
-//			//Serial.println(value_dac,DEC);
+//			//Serial.print("value_dac[axis] ");
+//			//Serial.println(value_dac[axis],DEC);
 //			//
 //			//Serial.print("deltaPosition ");
 //			//Serial.println(deltaPosition,DEC);
@@ -105,7 +105,7 @@ double data_polyFit(int axis, double position_input )
 //	}
 //	while (abs(deltaPosition)>max_step_size);
 //
-//	return value_dac;
+//	return value_dac[axis];
 //}
 //uint32_t piezo_predict_Position2Voltage_DAC_variant_step_number(int axis,double PositionInput,int steps)
 //	// input output range 0~1
@@ -129,7 +129,7 @@ double data_polyFit(int axis, double position_input )
 //	double deltaPosition = PositionInput - PositionNow[axis];
 //	double temp_PositionInput=PositionInput;
 //	double model_outputV=0;
-//	uint32_t value_dac=0;
+//	uint32_t value_dac[axis]=0;
 //	//double max_step_size=max(MAX_STEP_SIZE_PIEZO_MODEL,(deltaPosition/MAX_STEPS_PIEZO_MODEL+0.001));
 //	double max_step_size=max(MAX_STEP_SIZE_PIEZO_MODEL,(deltaPosition/steps+0.001));
 //
@@ -173,12 +173,12 @@ double data_polyFit(int axis, double position_input )
 //
 //		deltaPosition = PositionInput - PositionNow[axis];
 //		//if (axis>0)
-//		value_dac=(uint32_t)(model_outputV*(double)BIT18MAX);
-//		DAC_write(axis, value_dac);
+//		value_dac[axis]=(uint32_t)(model_outputV*(double)BIT18MAX);
+//		DAC_write(axis, value_dac[axis]);
 //
 //
-//		//Serial.print("value_dac ");
-//		//Serial.println(value_dac,DEC);
+//		//Serial.print("value_dac[axis] ");
+//		//Serial.println(value_dac[axis],DEC);
 //		//
 //		//Serial.print("deltaPosition ");
 //		//Serial.println(deltaPosition,DEC);
@@ -186,114 +186,114 @@ double data_polyFit(int axis, double position_input )
 //	}
 //	while (abs(deltaPosition)>max_step_size);
 //
-//	return value_dac;
+//	return value_dac[axis];
 //}
 
-uint32_t piezo_predict_Position01_To_Voltage_DAC18(int axis,double PositionInput,uint32_t* pV18_Adc_value)//,int steps
-	// input output range 0~1
-	// this function is non-re_enter_able
-{	//alpha: 0.4000
-	// beta: 9.1500
-	//gamma: 1.5000
-	//   dp: 1.0030
-	//    n: 1
-	//	pmdl.alpha = 0.35;
-	//pmdl.beta = 7;
-	//pmdl.gamma = 4;
-	//pmdl.dp = 1.09;
-	//pmdl.n = 1.02;
-	const	double alpha=0.35,beta=7,gamma=4,dp=1.09;
+//uint32_t piezo_predict_Position01_To_Voltage_DAC18(int axis,double PositionInput,uint32_t* pV18_Adc_value)//,int steps
+//	// input output range 0~1
+//	// this function is non-re_enter_able
+//{	//alpha: 0.4000
+//	// beta: 9.1500
+//	//gamma: 1.5000
+//	//   dp: 1.0030
+//	//    n: 1
+//	//	pmdl.alpha = 0.35;
+//	//pmdl.beta = 7;
+//	//pmdl.gamma = 4;
+//	//pmdl.dp = 1.09;
+//	//pmdl.n = 1.02;
+//	const	double alpha=0.35,beta=7,gamma=4,dp=1.09;
+//
+//	static double H_now[NUM_OF_PIEZO_MODEL]={0};
+//	static double PositionNow[NUM_OF_PIEZO_MODEL]={0};
+//
+//	double deltaPosition = PositionInput - PositionNow[axis];
+//	double temp_PositionInput=PositionInput;
+//	double model_outputV=0;
+//	uint32_t value_dac[axis]=0;
+//	double temp_deltaPosition=0;
+//	double dH=0;
+//
+//	while (abs(deltaPosition)>EPS)// 85us each round
+//	{
+//		//fastDigitalWrite(23,true);
+//		if (abs(deltaPosition)<MAX_STEP_SIZE_PIEZO_MODEL)
+//			temp_deltaPosition=deltaPosition;// last step
+//		else// equal steps
+//		{
+//			if (deltaPosition>0)// move direction
+//				temp_deltaPosition=MAX_STEP_SIZE_PIEZO_MODEL;
+//			else
+//				temp_deltaPosition=-MAX_STEP_SIZE_PIEZO_MODEL;
+//		}
+//		temp_PositionInput=PositionNow[axis]+temp_deltaPosition;
+//		deltaPosition-=temp_deltaPosition;
+//		//double H_now = H_now;
+//		//H_now = H_now.^n;
+//		dH = (alpha/dp*temp_deltaPosition - beta*H_now[axis]*temp_deltaPosition*SIGN(temp_deltaPosition) - gamma*abs(H_now[axis])*temp_deltaPosition) /
+//			(1-alpha/dp+beta*H_now[axis]*SIGN(temp_deltaPosition)+gamma*abs(H_now[axis]) );
+//		//	% dH = alpha*dV-beta*abs(dV)*H(k)-gamma*dV*abs(H(k));   //   % dV * (alpha - (gamma + beta * SIGN(dV * H)) * abs(H)^n);
+//		H_now[axis] += dH;
+//		PositionNow[axis] = temp_PositionInput;
+//		//	% y(k) = dp.*ExcitationVoltage(k) -H_now;
+//		//ExcitationVoltage=(temp_PositionInput+H_now)/dp;
+//		model_outputV=(temp_PositionInput+H_now[axis])/dp;
+//
+//		//Serial.print("dH ");
+//		//Serial.println(dH,DEC);
+//		//Serial.print("H_now[axis] ");
+//		//Serial.println(H_now[axis],DEC);
+//
+//		//model_outputV+= data_polyFit(axis, temp_PositionInput)-temp_PositionInput;
+//
+//		//Serial.print("model_outputV after");
+//		//Serial.println(model_outputV,DEC);
+//
+//		model_outputV=Max(model_outputV,0);
+//		model_outputV=Min(model_outputV,1);
+//
+//		//deltaPosition = PositionInput - PositionNow[axis];
+//		//if (axis>0)
+//		value_dac[axis]=(uint32_t)(model_outputV*(double)BIT18MAX);
+//		//DAC_write(axis, value_dac[axis]);		
+//		ADC_read_DAC_write(axis,pV18_Adc_value,axis,value_dac[axis]);// speed up for both read and write
+//		//fastDigitalWrite(23,false);
+//		//delay(1);
+//	}
+//
+//	//Serial.print("value_dac[axis] ");
+//	//Serial.println(value_dac[axis],DEC);
+//	//
+//	//Serial.print("deltaPosition ");
+//	//Serial.println(deltaPosition,DEC);
+//	//fastDigitalWrite(23,false);
+//
+//	//		while (abs(deltaPosition)>max_step_size);
+//
+//	// model reset
+//	if (PositionInput<=EPS)
+//	{
+//		H_now[axis]=0;
+//		PositionNow[axis]=0;
+//		value_dac[axis]=0;
+//		//DAC_write(axis, value_dac[axis]);
+//		ADC_read_DAC_write(axis,pV18_Adc_value,axis,value_dac[axis]);// speed up for both read and write
+//	}
+//	return value_dac[axis];
+//}
 
-	static double H_now[NUM_OF_PIEZO_MODEL]={0};
-	static double PositionNow[NUM_OF_PIEZO_MODEL]={0};
-
-	double deltaPosition = PositionInput - PositionNow[axis];
-	double temp_PositionInput=PositionInput;
-	double model_outputV=0;
-	uint32_t value_dac=0;
-	double temp_deltaPosition=0;
-	double dH=0;
-
-	while (abs(deltaPosition)>EPS)// 85us each round
-	{
-		//fastDigitalWrite(23,true);
-		if (abs(deltaPosition)<MAX_STEP_SIZE_PIEZO_MODEL)
-			temp_deltaPosition=deltaPosition;// last step
-		else// equal steps
-		{
-			if (deltaPosition>0)// move direction
-				temp_deltaPosition=MAX_STEP_SIZE_PIEZO_MODEL;
-			else
-				temp_deltaPosition=-MAX_STEP_SIZE_PIEZO_MODEL;
-		}
-		temp_PositionInput=PositionNow[axis]+temp_deltaPosition;
-		deltaPosition-=temp_deltaPosition;
-		//double H_now = H_now;
-		//H_now = H_now.^n;
-		dH = (alpha/dp*temp_deltaPosition - beta*H_now[axis]*temp_deltaPosition*SIGN(temp_deltaPosition) - gamma*abs(H_now[axis])*temp_deltaPosition) /
-			(1-alpha/dp+beta*H_now[axis]*SIGN(temp_deltaPosition)+gamma*abs(H_now[axis]) );
-		//	% dH = alpha*dV-beta*abs(dV)*H(k)-gamma*dV*abs(H(k));   //   % dV * (alpha - (gamma + beta * SIGN(dV * H)) * abs(H)^n);
-		H_now[axis] += dH;
-		PositionNow[axis] = temp_PositionInput;
-		//	% y(k) = dp.*ExcitationVoltage(k) -H_now;
-		//ExcitationVoltage=(temp_PositionInput+H_now)/dp;
-		model_outputV=(temp_PositionInput+H_now[axis])/dp;
-
-		//Serial.print("dH ");
-		//Serial.println(dH,DEC);
-		//Serial.print("H_now[axis] ");
-		//Serial.println(H_now[axis],DEC);
-
-		//model_outputV+= data_polyFit(axis, temp_PositionInput)-temp_PositionInput;
-
-		//Serial.print("model_outputV after");
-		//Serial.println(model_outputV,DEC);
-
-		model_outputV=Max(model_outputV,0);
-		model_outputV=Min(model_outputV,1);
-
-		//deltaPosition = PositionInput - PositionNow[axis];
-		//if (axis>0)
-		value_dac=(uint32_t)(model_outputV*(double)BIT18MAX);
-		//DAC_write(axis, value_dac);		
-		ADC_read_DAC_write(axis,pV18_Adc_value,axis,value_dac);// speed up for both read and write
-		//fastDigitalWrite(23,false);
-		//delay(1);
-	}
-
-	//Serial.print("value_dac ");
-	//Serial.println(value_dac,DEC);
-	//
-	//Serial.print("deltaPosition ");
-	//Serial.println(deltaPosition,DEC);
-	//fastDigitalWrite(23,false);
-
-	//		while (abs(deltaPosition)>max_step_size);
-	
-	// model reset
-	if (PositionInput<=EPS)
-	{
-		H_now[axis]=0;
-		PositionNow[axis]=0;
-		value_dac=0;
-		//DAC_write(axis, value_dac);
-		ADC_read_DAC_write(axis,pV18_Adc_value,axis,value_dac);// speed up for both read and write
-	}
-	return value_dac;
-}
-
-uint32_t piezo_predict_Position01_To_Voltage_DAC18_compatible(int axis,double PositionInput)//,int steps
-	// input output range 0~1
-	// this function is non-re_enter_able
-{
-	uint32_t V18_Adc_value=0;// not used
-	return piezo_predict_Position01_To_Voltage_DAC18(axis,PositionInput,&V18_Adc_value);
-}
+//uint32_t piezo_predict_Position01_To_Voltage_DAC18_compatible(int axis,double PositionInput)//,int steps
+//	// input output range 0~1
+//	// this function is non-re_enter_able
+//{
+//	uint32_t V18_Adc_value=0;// not used
+//	return piezo_predict_Position01_To_Voltage_DAC18(axis,PositionInput,&V18_Adc_value);
+//}
 
 uint32_t piezo_predict_Position01_To_Voltage_DAC18(int axis,double PositionInput)//,int steps
 {	// input output range 0~1
 	// this function is non-re_enter_able
-	
+
 	//alpha: 0.4000
 	// beta: 9.1500
 	//gamma: 1.5000
@@ -308,84 +308,81 @@ uint32_t piezo_predict_Position01_To_Voltage_DAC18(int axis,double PositionInput
 
 	static double H_now[NUM_OF_PIEZO_MODEL]={0};
 	static double PositionNow[NUM_OF_PIEZO_MODEL]={0};
+	static uint32_t value_dac[NUM_OF_PIEZO_MODEL]={0};// save the value for output when input is the same as current state;// display bug fixed, 20151030, add static
 	
 	double deltaPosition = PositionInput - PositionNow[axis];
 	double temp_PositionInput=PositionInput;
 	double model_outputV=0;
-	uint32_t value_dac=0;
-	double temp_deltaPosition=0;
+double temp_deltaPosition=0;
 	double dH=0;	
-	
+
 	//////////////////////////////////////////////////////////// reset model
 	// model reset
 	if (PositionInput<=EPS)
 	{
 		H_now[axis]=0;
 		PositionNow[axis]=0;
-		value_dac=0;
-		DAC_write(axis, value_dac);
-		//ADC_read_DAC_write(axis,pV18_Adc_value,axis,value_dac);// speed up for both read and write
-		return value_dac;
+		value_dac[axis]=0;
+		DAC_write(axis, value_dac[axis]);
+		//ADC_read_DAC_write(axis,pV18_Adc_value,axis,value_dac[axis]);// speed up for both read and write
 	}
-
-	////////////////////////////////////////////////////////////// normail feedforward
-
-	while (abs(deltaPosition)>EPS)// 85us each round
-	{
-		//fastDigitalWrite(23,true);
-		if (abs(deltaPosition)<MAX_STEP_SIZE_PIEZO_MODEL)
-			temp_deltaPosition=deltaPosition;// last step
-		else// equal steps
+	else
+	{	////////////////////////////////////////////////////////////// normail feedforward
+		while (abs(deltaPosition)>EPS)// 85us each round
 		{
-			if (deltaPosition>0)// move direction
-				temp_deltaPosition=MAX_STEP_SIZE_PIEZO_MODEL;
-			else
-				temp_deltaPosition=-MAX_STEP_SIZE_PIEZO_MODEL;
+			//fastDigitalWrite(23,true);
+			if (abs(deltaPosition)<MAX_STEP_SIZE_PIEZO_MODEL)
+				temp_deltaPosition=deltaPosition;// last step
+			else// equal steps
+			{
+				if (deltaPosition>0)// move direction
+					temp_deltaPosition=MAX_STEP_SIZE_PIEZO_MODEL;
+				else
+					temp_deltaPosition=-MAX_STEP_SIZE_PIEZO_MODEL;
+			}
+			temp_PositionInput=PositionNow[axis]+temp_deltaPosition;
+			deltaPosition-=temp_deltaPosition;
+			//double H_now = H_now;
+			//H_now = H_now.^n;
+			dH = (alpha/dp*temp_deltaPosition - beta*H_now[axis]*temp_deltaPosition*SIGN(temp_deltaPosition) - gamma*abs(H_now[axis])*temp_deltaPosition) /
+				(1-alpha/dp+beta*H_now[axis]*SIGN(temp_deltaPosition)+gamma*abs(H_now[axis]) );
+			//	% dH = alpha*dV-beta*abs(dV)*H(k)-gamma*dV*abs(H(k));   //   % dV * (alpha - (gamma + beta * SIGN(dV * H)) * abs(H)^n);
+			H_now[axis] += dH;
+			PositionNow[axis] = temp_PositionInput;
+			//	% y(k) = dp.*ExcitationVoltage(k) -H_now;
+			//ExcitationVoltage=(temp_PositionInput+H_now)/dp;
+			model_outputV=(temp_PositionInput+H_now[axis])/dp;
+
+			//Serial.print("dH ");
+			//Serial.println(dH,DEC);
+			//Serial.print("H_now[axis] ");
+			//Serial.println(H_now[axis],DEC);
+
+			//model_outputV+= data_polyFit(axis, temp_PositionInput)-temp_PositionInput;
+
+			//Serial.print("model_outputV after");
+			//Serial.println(model_outputV,DEC);
+
+			model_outputV=Max(model_outputV,0.0);
+			model_outputV=Min(model_outputV,1.0);
+
+			//deltaPosition = PositionInput - PositionNow[axis];
+			//if (axis>0)
+			value_dac[axis]=(uint32_t)(model_outputV*(double)BIT18MAX);
+			DAC_write(axis, value_dac[axis]);		
+			//ADC_read_DAC_write(axis,pV18_Adc_value,axis,value_dac[axis]);// speed up for both read and write
+			//fastDigitalWrite(23,false);
+			//delay(1);
 		}
-		temp_PositionInput=PositionNow[axis]+temp_deltaPosition;
-		deltaPosition-=temp_deltaPosition;
-		//double H_now = H_now;
-		//H_now = H_now.^n;
-		dH = (alpha/dp*temp_deltaPosition - beta*H_now[axis]*temp_deltaPosition*SIGN(temp_deltaPosition) - gamma*abs(H_now[axis])*temp_deltaPosition) /
-			(1-alpha/dp+beta*H_now[axis]*SIGN(temp_deltaPosition)+gamma*abs(H_now[axis]) );
-		//	% dH = alpha*dV-beta*abs(dV)*H(k)-gamma*dV*abs(H(k));   //   % dV * (alpha - (gamma + beta * SIGN(dV * H)) * abs(H)^n);
-		H_now[axis] += dH;
-		PositionNow[axis] = temp_PositionInput;
-		//	% y(k) = dp.*ExcitationVoltage(k) -H_now;
-		//ExcitationVoltage=(temp_PositionInput+H_now)/dp;
-		model_outputV=(temp_PositionInput+H_now[axis])/dp;
-
-		//Serial.print("dH ");
-		//Serial.println(dH,DEC);
-		//Serial.print("H_now[axis] ");
-		//Serial.println(H_now[axis],DEC);
-
-		//model_outputV+= data_polyFit(axis, temp_PositionInput)-temp_PositionInput;
-
-		//Serial.print("model_outputV after");
-		//Serial.println(model_outputV,DEC);
-
-		model_outputV=Max(model_outputV,0);
-		model_outputV=Min(model_outputV,1);
-
-		//deltaPosition = PositionInput - PositionNow[axis];
-		//if (axis>0)
-		value_dac=(uint32_t)(model_outputV*(double)BIT18MAX);
-		DAC_write(axis, value_dac);		
-		//ADC_read_DAC_write(axis,pV18_Adc_value,axis,value_dac);// speed up for both read and write
-		//fastDigitalWrite(23,false);
-		//delay(1);
 	}
+	return value_dac[axis];// bug fix, 20151030
+}	
+//Serial.print("value_dac[axis] ");
+//Serial.println(value_dac[axis],DEC);
+//
+//Serial.print("deltaPosition ");
+//Serial.println(deltaPosition,DEC);
+//fastDigitalWrite(23,false);
 
-	//Serial.print("value_dac ");
-	//Serial.println(value_dac,DEC);
-	//
-	//Serial.print("deltaPosition ");
-	//Serial.println(deltaPosition,DEC);
-	//fastDigitalWrite(23,false);
-
-	//		while (abs(deltaPosition)>max_step_size);
-	
-
-}
+//		while (abs(deltaPosition)>max_step_size);
 #endif
